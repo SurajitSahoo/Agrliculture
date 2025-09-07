d8ab6fdd387ecb1845bfeecf78351263cf56bbd1
Surajit Sahoo <surajitsahoo200@gmail.com>
2025-09-08 00:37:13 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 import Nav from './component/Nav.js';
-
+import Cards from './component/Cards.js'; 
 function App() {
   return (
     <div className="background-container">
       <Nav/>
-      
+      <Cards/>
     </div>
   );
 }
---
+++ src/component/Cards.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Card,CardBody,CardText,CardTitle,CardImg} from "reactstrap";
+import {Button} from "reactstrap";
+import './Cards.css';
+const Cards =()=>
+{
+    return(
+    <div style={{ display: "flex", flexWrap: "wrap"}} className="cards">
+        <div className="card1">
+            <Card style={{ width: '15rem' }} className="bg-grey ">
+                <CardImg className="c1" varient='top' src="https://images.unsplash.com/photo-1437252611977-07f74518abd7?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+                <CardBody>
+                    <CardTitle>Wheat</CardTitle>
+                    <CardText>Here you can find out your Wheat price and which types of wheat is more valuable</CardText>
+                
+                <Button color="primary">Show Details</Button>{" "}
+                 <Button color="warning">Price</Button>
+                 
+                </CardBody>
+            </Card>
+
+        </div>
+        <div className="card2">
+            <Card style={{ width: '16rem' }} className="bg-grey ">
+                <CardImg className="c2" varient='top' src="https://cdn.jwplayer.com/v2/media/HfJdJZ0V/poster.jpg?width=1280"/>
+                <CardBody>
+                    <CardTitle>White Rice</CardTitle>
+                    <CardText>Here you can find out your White rice price and which types of rice is more valuable</CardText>
+                <Button color="primary">Show Details</Button>{" "}
+                <Button color="warning">Price</Button>
+                </CardBody>
+            </Card>
+        </div></div>  
+        
+      
+    )
+}
+export default Cards;
